1680ed5e4cb78d12a1ed90576a36e735c5ae2c36
Rhanel Candia <rhancandia@gmail.com>
2016-09-23 03:21:23 +0800

    fixed side nav scroll

--- src/app/navbar/navbar.js
+++ src/app/navbar/navbar.js
@@ -2,11 +2,24 @@ angular
   .module('app')
   .component('navbar', {
     templateUrl: 'app/navbar/navbar.html',
-    controller: function ($mdSidenav) {
+    controller: function ($mdSidenav, $document) {
       var self = this;
 
       self.toggleSideNav = function () {
         $mdSidenav('side-nav').toggle();
+        var $body = angular.element($document[0].body);
+
+        if ($mdSidenav('side-nav').isOpen()) {
+            $body.css({
+              'overflow': 'hidden'
+            });
+        }
+
+        $mdSidenav('side-nav').onClose(function () {
+          $body.css({
+            'overflow': 'auto'
+          });
+        });
       };
     }
   });
